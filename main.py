3f3919218cd308f57b8cf082ae523b7654b3bb84
mlee108 <18340464+mlee108@users.noreply.github.com>
2017-11-08 12:58:18 -0600

    made some ui changes

--- main.py
+++ main.py
@@ -10,26 +10,18 @@ app.config['UPLOADED_PHOTOS_ALLOW'] = set(['png', 'jpg', 'jpeg'])
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 configure_uploads(app, photos)
 
-
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
     return render_template('main.html')
 
-@app.route('/upload', methods=['GET', 'POST'])
+@app.route('/', methods=['POST'])
 def upload():
-    if request.method == 'POST' and 'photo' in request.files:
-        print "19"
+    if 'photo' in request.files:
         print request.files['photo']
-        photos.save(request.files['photo'], 'subfolder_test', 'filename_test.png')
+        photos.save(request.files['photo'], 'uploads', 'filename_test.png')
 
     return render_template('main.html')
 
-
-
-
 if __name__ == "__main__":
-
     app.debug = True
     app.run()
-
-
---
+++ readme.txt
@@ -0,0 +1,6 @@
+pip install flask
+pip install flask-uploads
+
+$env:FLASK_APP = "main.py" (Windows Powershell)
+export FLASK_APP=main.py (Mac)
+flask run
