1b5f44579f59bae10fa50967bede6531ffa95cb8
mlee108 <18340464+mlee108@users.noreply.github.com>
2017-11-08 18:42:51 -0600

    fixed the erorr

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SugarSugar
+
+pip install flask
+pip install flask-uploads
+
+$env:FLASK_APP = "main.py" (Windows Powershell)
+export FLASK_APP=main.py (Mac)
+flask run
--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from flask import Flask, render_template, request
-from flask.ext.uploads import UploadSet, configure_uploads, IMAGES
+from flask_uploads import UploadSet, configure_uploads, IMAGES
 
 app = Flask(__name__, static_url_path = '')
 
